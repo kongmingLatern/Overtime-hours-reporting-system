2eacd13567b251dbb0bf9530663384911ee1b89f
凤之兮原 <1577310596@qq.com>
2023-07-04 20:29:31 +0800

    chore: add unocss

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import Antd from "ant-design-vue";
 import router from "@/router";
 import "ant-design-vue/dist/antd.css";
+import "uno.css";
 
 const app = createApp(App);
 app.use(createPinia());
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJSX from "@vitejs/plugin-vue-jsx";
+import UnoCSS from "unocss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [vue(), vueJSX()],
+  plugins: [vue(), vueJSX(), UnoCSS()],
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
