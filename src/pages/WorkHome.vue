e537d147402cb60d1f1889ae8636de12629dd80e
凤之兮原 <1577310596@qq.com>
2023-07-04 17:01:17 +0800

    feat: finish setup

--- src/pages/WorkHome.vue
+++ src/pages/WorkHome.vue
@@ -1,9 +1,11 @@
 <template>
   <WorkTable />
+  <WorkTableA />
 </template>
 
 <script setup lang="ts">
 import WorkTable from "@/components/WorkTable.vue";
+import WorkTableA from "@/components/A.tsx";
 </script>
 
 <style scoped></style>
