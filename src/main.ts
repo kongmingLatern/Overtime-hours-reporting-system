6da22ed3f00b6bf32bf3b722dd43cd82440bf030
凤之兮原 <1577310596@qq.com>
2023-07-04 15:59:01 +0800

    feat: add vitest jsdom for test and add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 
-createApp(App).use(Antd).mount("#app");
+createApp(App).use(Antd).use(createPinia()).mount("#app");
