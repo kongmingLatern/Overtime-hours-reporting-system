e4fc0c87e94a4db4f5b779830fbb9c1752e3c68a
凤之兮原 <1577310596@qq.com>
2023-07-05 20:19:14 +0800

    feat: add Basic Layout

---
+++ src/layout/Basic.tsx
@@ -0,0 +1,35 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup(_, { slots }) {
+    const Slot = {
+      header: slots?.header,
+      content: slots?.content,
+      footer: slots?.footer,
+      sider: slots?.sider,
+    };
+    return () => (
+      <aLayout>
+        <aLayoutSider
+          bg="#f0f2f5"
+          style={{
+            height: "100vh",
+          }}
+        >
+          {Slot.sider && Slot.sider()}
+        </aLayoutSider>
+        <aLayout>
+          <a-layout-header bg="#f0f2f5" p="0">
+            {Slot.header && Slot.header()}
+          </a-layout-header>
+          <a-layout-content bg="#f0f2f5" p="0">
+            {Slot.content && Slot.content()}
+          </a-layout-content>
+          <a-layout-footer bg="#f0f2f5" p="0">
+            {Slot.footer && Slot.footer()}
+          </a-layout-footer>
+        </aLayout>
+      </aLayout>
+    );
+  },
+});
--- src/pages/WorkHome.vue
+++ src/pages/WorkHome.vue
@@ -1,19 +1,47 @@
 <template>
-  <ModalButtonTSX
+  <Basic>
+    <template #sider>
+      <div color="blue" h-full>Sider</div>
+    </template>
+    <template #header>
+      <h1 bg="blue-400" h-full>Header</h1>
+    </template>
+    <template #content>
+      <div bg="red-400" h-full>Content</div>
+    </template>
+    <template #footer>
+      <div bg="green-400" h-full>Footer</div>
+    </template>
+  </Basic>
+
+  <!-- <ModalButtonTSX
     :form-state="formState"
     :rule-state="ruleState"
     button-text="button"
     cancel-text="cancel"
     ok-text="ok"
     title="TestDemo"
+    :on-ok="onOk"
+    :on-fail="onFail"
   />
   <CascaderSelect :options="options" :on-change="onChange" />
+  <DeleteButton
+    value="delete"
+    cancel-text="取消"
+    ok-text="确认"
+    :on-confirm="onConfirm"
+  >
+    <Icon icon="material-symbols:10k" />
+  </DeleteButton> -->
 </template>
 
 <script setup lang="ts">
 import ModalButtonTSX from "@/components/ModalButton.tsx";
 import CascaderSelect from "@/components/CascaderSelect.tsx";
+import DeleteButton from "@/components/DeleteButton.tsx";
+import Basic from "@/layout/basic";
 import { CascaderProps } from "ant-design-vue";
+import { Icon } from "@iconify/vue";
 
 const formState = {
   name: "",
@@ -90,4 +118,14 @@ const options: CascaderProps["options"] = [
 const onChange = (value: string[]) => {
   console.log("value", value);
 };
+const onConfirm = (e) => {
+  console.log(e);
+};
+const onOk = (value) => {
+  console.log("onOk", value);
+};
+const onFail = (info) => {
+  console.log("onFail", info);
+};
 </script>
+@/layout/Basic
