4878c219e416355878d89318c8f92797ed804771
凤之兮原 <1577310596@qq.com>
2023-07-05 20:23:23 +0800

    chore: add rule for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,5 +29,6 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-function": "off",
+    "vue/multi-word-component-names": "off",
   },
 };
