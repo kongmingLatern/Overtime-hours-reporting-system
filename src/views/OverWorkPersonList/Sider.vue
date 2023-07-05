448bffb6e04bf627c84152444f769be5fb65b992
凤之兮原 <1577310596@qq.com>
2023-07-05 21:11:13 +0800

    feat: finish Left Sider view

--- src/views/OverWorkPersonList/Sider.vue
+++ src/views/OverWorkPersonList/Sider.vue
@@ -1,27 +1,52 @@
 <template>
-  <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+  <a-carousel autoplay :dots="false" p="5">
+    <div text="center" bg="#001529" p="2"><h3 color="white">1</h3></div>
+    <div text="center" bg="#001529" p="2"><h3 color="white">2</h3></div>
+    <div text="center" bg="#001529" p="2"><h3 color="white">3</h3></div>
+    <div text="center" bg="#001529" p="2"><h3 color="white">4</h3></div>
+  </a-carousel>
+  <a-menu
+    v-model:selectedKeys="selectedKeys"
+    theme="dark"
+    mode="inline"
+    h="100vh"
+  >
     <a-menu-item key="1">
-      <user-outlined />
-      <span class="nav-text">nav 1</span>
+      <a-space>
+        <Icon flex icon="ic:round-person" width="20" />
+        <span class="nav-text">加班人员列表</span>
+      </a-space>
     </a-menu-item>
     <a-menu-item key="2">
-      <video-camera-outlined />
-      <span class="nav-text">nav 2</span>
+      <a-space>
+        <Icon flex icon="solar:list-broken" width="20" />
+        <span class="nav-text">人员查询</span>
+      </a-space>
     </a-menu-item>
     <a-menu-item key="3">
-      <upload-outlined />
-      <span class="nav-text">nav 3</span>
+      <a-space>
+        <Icon flex icon="mingcute:department-fill" width="20" />
+        <span class="nav-text">部门查询</span>
+      </a-space>
     </a-menu-item>
     <a-menu-item key="4">
-      <user-outlined />
-      <span class="nav-text">nav 4</span>
+      <a-space>
+        <Icon flex icon="ant-design:project-outlined" width="20" />
+        <span class="nav-text">项目维护</span>
+      </a-space>
+    </a-menu-item>
+    <a-menu-item key="5">
+      <a-space>
+        <Icon flex icon="lucide:sheet" width="20" />
+        <span class="nav-text">生成报表</span>
+      </a-space>
     </a-menu-item>
   </a-menu>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-
+import { Icon } from "@iconify/vue";
 const selectedKeys = ref<string[]>(["4"]);
 </script>
 
