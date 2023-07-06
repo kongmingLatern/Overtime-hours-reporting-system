3caff550c58bec00eb2e1ba5b426e268c14b0f90
凤之兮原 <1577310596@qq.com>
2023-07-06 16:39:43 +0800

    chore: add Footer for Basic Layout

--- src/views/OverWorkPersonList/Footer.vue
+++ src/views/OverWorkPersonList/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div bg="green-400" h-full>Footer</div>
+  <div bg="blue-300" p="5" text="center lg white" font="bold">
+    &copy; 2023-2024 Syj & Zmt
+  </div>
 </template>
 
 <script setup lang="ts"></script>
