351638dbf5c30712faef19f5bfdc47918fe0b46b
凤之兮原 <1577310596@qq.com>
2023-07-06 15:16:25 +0800

    feat: update Tag status

--- src/components/common/CustomTable.tsx
+++ src/components/common/CustomTable.tsx
@@ -15,6 +15,17 @@ export default defineComponent({
     const handleSlot = {
       //  对应 operation 的单元格 插槽内容
       bodyCell: ({ text, column }) => {
+        if (column.key === "status") {
+          switch (text) {
+            case "已通过":
+              return <aTag color="green">{text}</aTag>;
+            case "已提交":
+              return <aTag color="blue">{text}</aTag>;
+            case "已驳回":
+              return <aTag color="red">{text}</aTag>;
+          }
+        }
+
         if (column.key === "operation") {
           return (
             <slot name="operation">
