d050a20b382f4a8deb7df1244485e8f453ce2320
凤之兮原 <1577310596@qq.com>
2023-07-07 13:50:59 +0800

    feat: add Echarts and BarEchart component

---
+++ src/components/common/BarEchart.vue
@@ -0,0 +1,38 @@
+<template>
+  <e-chart class="chart" :option="option" />
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+const data = ref([
+  { value: 67, name: "A" },
+  { value: 40, name: "B" },
+  { value: 120, name: "C" },
+  { value: 58, name: "D" },
+  { value: 85, name: "E" },
+]);
+
+const option = computed(() => {
+  return {
+    xAxis: {
+      type: "category",
+      data: data.value.map((el) => el.name),
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: data.value.map((el) => el.value),
+        type: "line",
+      },
+    ],
+  };
+});
+</script>
+
+<style scoped>
+.chart {
+  height: 400px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
+import { createApp } from "vue";
+
+// ======== Pinia ========
+import { createPinia } from "pinia";
+
+// ======== Ant Design Vue ========
 import Antd from "ant-design-vue";
-import router from "@/router";
 import "ant-design-vue/dist/antd.css";
+
+// ======== UNO ========
 import "uno.css";
 import "@unocss/reset/normalize.css";
 
+// ======== Router ========
+import router from "./router";
+
+// ======== ECharts ========
+import ECharts from "vue-echarts";
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { BarChart, LineChart } from "echarts/charts";
+import { GridComponent, TooltipComponent } from "echarts/components";
+
+use([CanvasRenderer, BarChart, GridComponent, TooltipComponent, LineChart]);
+
 const app = createApp(App);
+app.component("e-chart", ECharts);
 app.use(createPinia());
 app.use(Antd);
 app.use(router);
