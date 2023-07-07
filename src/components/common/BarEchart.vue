64707a3f3c6acc63f876263285bfbed6af38147a
凤之兮原 <1577310596@qq.com>
2023-07-07 14:52:16 +0800

    chore: package barEchart

--- src/components/common/BarEchart.vue
+++ src/components/common/BarEchart.vue
@@ -1,38 +1,9 @@
 <template>
-  <e-chart class="chart" :option="option" />
+  <e-chart :option="props.option" />
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
-const data = ref([
-  { value: 67, name: "A" },
-  { value: 40, name: "B" },
-  { value: 120, name: "C" },
-  { value: 58, name: "D" },
-  { value: 85, name: "E" },
-]);
-
-const option = computed(() => {
-  return {
-    xAxis: {
-      type: "category",
-      data: data.value.map((el) => el.name),
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: data.value.map((el) => el.value),
-        type: "line",
-      },
-    ],
-  };
-});
+const props = defineProps<{
+  option: any;
+}>();
 </script>
-
-<style scoped>
-.chart {
-  height: 400px;
-}
-</style>
