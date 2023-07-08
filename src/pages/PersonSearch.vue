f4b22f82b115f5ffb6db1929923bbf5dcd7ef37f
凤之兮原 <1577310596@qq.com>
2023-07-08 16:02:52 +0800

    chore: format and change import

--- src/pages/PersonSearch.vue
+++ src/pages/PersonSearch.vue
@@ -16,11 +16,11 @@
   </RouterContent>
 </template>
 
-
 <script setup lang="ts">
 import RouterContent from "@/layout/RouterContent";
 import Header from "@/views/PersonSearch/Header.vue";
 import Content from "@/views/PersonSearch/Content.vue";
+import ToolBar from "@/views/PersonSearch/ToolBar.vue";
 import Footer from "@/views/PersonSearch/Footer.vue";
 </script>
 
--- src/views/PersonSearch/Content.vue
+++ src/views/PersonSearch/Content.vue
@@ -1,74 +1,71 @@
 <template>
-    <slot name="toolbar"></slot>
-    <CustomTable :columns="personSearchColumns" :data="personDataSource">
-      <template #operation="record">
-        <a-space>
-            <ModalButton
-            button-text="编辑"
-            title="编辑"
-            :form-state="record"
-            :rule-state="ruleState"
-            />
+  <slot name="toolbar"></slot>
+  <CustomTable :columns="personSearchColumns" :data="personDataSource">
+    <template #operation="record">
+      <a-space>
+        <ModalButton
+          button-text="编辑"
+          title="编辑"
+          :form-state="record"
+          :rule-state="ruleState"
+        />
 
-            <DeleteButton 
-                title="确定是否要删除"
-                okText="确定"
-            />
-        </a-space>
-      </template>
-    </CustomTable>
-  </template>
-  
-  <script setup lang="ts">
-  import CustomTable from "@/components/common/CustomTable";
-  import ModalButton from "@/components/common/ModalButton";
-  import DeleteButton from "@/components/common/DeleteButton";
-  import {personSearchColumns, personDataSource } from "@/store";
-  
-  const ruleState = {
-    job_number: {
-      type: "number",
-      label: "工号",
-      rules: [
-        {
-          required: true,
-          message: "请输入工号",
-          trigger: "blur",
-        },
-      ],
-    },
-    job_name: {
-      type: "string",
-      label: "姓名",
-      rules: [
-        {
-          required: true,
-          message: "请输入姓名",
-          trigger: "blur",
-        },
-      ],
-    },
-    department: {
-      type: "string",
-      label: "所属部门",
-      rules: [
-        {
-          required: true,
-          message: "请输入所属部门",
-          trigger: "blur",
-        },
-      ],
-    },
-    job: {
-      type: "string",
-      label: "职级",
-      rules: [
-        {
-          required: true,
-          message: "请输入职级",
-          trigger: "blur",
-        },
-      ],
-    },  
-  };
-  </script>
+        <DeleteButton title="确定是否要删除" okText="确定" />
+      </a-space>
+    </template>
+  </CustomTable>
+</template>
+
+<script setup lang="ts">
+import CustomTable from "@/components/common/CustomTable";
+import ModalButton from "@/components/common/ModalButton";
+import DeleteButton from "@/components/common/DeleteButton";
+import { personSearchColumns, personDataSource } from "@/store";
+
+const ruleState = {
+  job_number: {
+    type: "number",
+    label: "工号",
+    rules: [
+      {
+        required: true,
+        message: "请输入工号",
+        trigger: "blur",
+      },
+    ],
+  },
+  job_name: {
+    type: "string",
+    label: "姓名",
+    rules: [
+      {
+        required: true,
+        message: "请输入姓名",
+        trigger: "blur",
+      },
+    ],
+  },
+  department: {
+    type: "string",
+    label: "所属部门",
+    rules: [
+      {
+        required: true,
+        message: "请输入所属部门",
+        trigger: "blur",
+      },
+    ],
+  },
+  job: {
+    type: "string",
+    label: "职级",
+    rules: [
+      {
+        required: true,
+        message: "请输入职级",
+        trigger: "blur",
+      },
+    ],
+  },
+};
+</script>
