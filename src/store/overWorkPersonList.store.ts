585c61f4d1b92b3eb7f5d3b0fae1075b6dbbb1d0
凤之兮原 <1577310596@qq.com>
2023-07-08 14:52:13 +0800

    refactor: refactor overWorkPersonList

--- src/store/overWorkPersonList.store.ts
+++ src/store/overWorkPersonList.store.ts
@@ -68,6 +68,104 @@ export const useOverWorkPersonListStore = defineStore(
       },
     ];
 
+    const ruleState = {
+      job_number: {
+        type: "number",
+        label: "工号",
+        rules: [
+          {
+            required: true,
+            message: "请输入工号",
+            trigger: "blur",
+          },
+        ],
+      },
+      job_name: {
+        type: "string",
+        label: "姓名",
+        rules: [
+          {
+            required: true,
+            message: "请输入姓名",
+            trigger: "blur",
+          },
+        ],
+      },
+      department: {
+        type: "string",
+        label: "所属部门",
+        rules: [
+          {
+            required: true,
+            message: "请输入所属部门",
+            trigger: "blur",
+          },
+        ],
+      },
+      status: {
+        type: "string",
+        label: "表单状态",
+        rules: [
+          {
+            required: true,
+            message: "请输入表单状态",
+            trigger: "blur",
+          },
+        ],
+      },
+      start_time: {
+        type: "date",
+        label: "加班开始时间",
+        rules: [
+          {
+            required: true,
+            message: "请输入加班开始时间",
+            trigger: "blur",
+          },
+        ],
+      },
+      end_time: {
+        type: "date",
+        label: "加班结束时间",
+        rules: [
+          {
+            required: true,
+            message: "请输入加班结束时间",
+            trigger: "blur",
+          },
+        ],
+      },
+      over_time: {
+        type: "text",
+        label: "加班时长",
+        rules: [
+          {
+            required: true,
+            message: "请输入加班时长",
+            trigger: "blur",
+          },
+        ],
+      },
+      report_time: {
+        type: "date",
+        label: "填报时间",
+        rules: [
+          {
+            required: true,
+            message: "请输入填报时间",
+            trigger: "blur",
+          },
+        ],
+      },
+      reason: {
+        type: "text",
+        label: "驳回原因",
+        options: {
+          disabled: true,
+        },
+      },
+    };
+
     async function getAllOverWorkPersonList() {
       const res = await http.get(AxiosAPIPath.GETALL);
       return res.data;
@@ -76,6 +174,7 @@ export const useOverWorkPersonListStore = defineStore(
     return {
       getAllOverWorkPersonList,
       columns,
+      ruleState,
     };
   }
 );
--- src/views/OverWorkPersonList/Content.vue
+++ src/views/OverWorkPersonList/Content.vue
@@ -16,6 +16,7 @@
         title="查看"
         :form-state="{ ...record, reason: '驳回原因' }"
         :rule-state="ruleState"
+        :button-props="{ danger: true }"
       />
       <a-button v-if="record.status === '已通过'" disabled>删除</a-button>
     </template>
@@ -30,109 +31,12 @@ import {
   useOverWorkPersonListStore,
   OverWorkPersonType,
 } from "@/store/overWorkPersonList.store";
-const { columns, getAllOverWorkPersonList } = useOverWorkPersonListStore();
+const { columns, ruleState, getAllOverWorkPersonList } =
+  useOverWorkPersonListStore();
 
 const dataSource = ref<OverWorkPersonType[]>([]);
 
 onMounted(async () => {
   dataSource.value = await getAllOverWorkPersonList();
 });
-
-const ruleState = {
-  job_number: {
-    type: "number",
-    label: "工号",
-    rules: [
-      {
-        required: true,
-        message: "请输入工号",
-        trigger: "blur",
-      },
-    ],
-  },
-  job_name: {
-    type: "string",
-    label: "姓名",
-    rules: [
-      {
-        required: true,
-        message: "请输入姓名",
-        trigger: "blur",
-      },
-    ],
-  },
-  department: {
-    type: "string",
-    label: "所属部门",
-    rules: [
-      {
-        required: true,
-        message: "请输入所属部门",
-        trigger: "blur",
-      },
-    ],
-  },
-  status: {
-    type: "string",
-    label: "表单状态",
-    rules: [
-      {
-        required: true,
-        message: "请输入表单状态",
-        trigger: "blur",
-      },
-    ],
-  },
-  start_time: {
-    type: "date",
-    label: "加班开始时间",
-    rules: [
-      {
-        required: true,
-        message: "请输入加班开始时间",
-        trigger: "blur",
-      },
-    ],
-  },
-  end_time: {
-    type: "date",
-    label: "加班结束时间",
-    rules: [
-      {
-        required: true,
-        message: "请输入加班结束时间",
-        trigger: "blur",
-      },
-    ],
-  },
-  over_time: {
-    type: "text",
-    label: "加班时长",
-    rules: [
-      {
-        required: true,
-        message: "请输入加班时长",
-        trigger: "blur",
-      },
-    ],
-  },
-  report_time: {
-    type: "date",
-    label: "填报时间",
-    rules: [
-      {
-        required: true,
-        message: "请输入填报时间",
-        trigger: "blur",
-      },
-    ],
-  },
-  reason: {
-    type: "text",
-    label: "驳回原因",
-    options: {
-      disabled: true,
-    },
-  },
-};
 </script>
