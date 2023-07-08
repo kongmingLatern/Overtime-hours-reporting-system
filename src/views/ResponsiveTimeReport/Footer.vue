b02408e0edae6a87ea213ea0e01e34818ed1952c
凤之兮原 <1577310596@qq.com>
2023-07-08 13:06:36 +0800

    chore: add Footer for ResponsiveTimeReport

---
+++ src/views/ResponsiveTimeReport/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div bg="blue-300" p="5" text="center lg white" font="bold">
+    &copy; 2023-2024 Syj & Zmt
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
