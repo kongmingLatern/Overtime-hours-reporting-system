2ba848036911c8c174dc31112d720624d23c0e31
凤之兮原 <1577310596@qq.com>
2023-07-12 09:13:29 +0800

    refactor: refactor RouterName

---
+++ src/router/RouterName.ts
@@ -0,0 +1,11 @@
+export const enum RouterName {
+  Admin = "admin",
+  OverWorkPersonList = "overWorkPersonList",
+  PersonSearch = "personSearch",
+  DepartmentSearch = "departmentSearch",
+  ProjectMaintain = "projectMaintain",
+  GenerateReports = "generateReports",
+  Home = "home",
+  Index = "index",
+  Login = "login",
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { RouterName } from "./RouterName";
+
 const routes = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,57 +10,57 @@ const routes = createRouter({
     },
     {
       path: "/admin",
-      name: "admin",
+      name: RouterName.Admin,
       redirect: "/admin/overWorkPersonList",
       component: () => import("@/layout/Basic"),
       children: [
         {
           // NOTE: 加班人员列表
           path: "overWorkPersonList",
-          name: "overWorkPersonList",
+          name: RouterName.OverWorkPersonList,
           component: () => import("@/pages/OverWorkPersonList.vue"),
         },
         {
           // NOTE: 员工查询
           path: "personSearch",
-          name: "personSearch",
+          name: RouterName.PersonSearch,
           component: () => import("@/pages/PersonSearch.vue"),
         },
         {
           // NOTE: 部门查询
           path: "departmentSearch",
-          name: "departmentSearch",
+          name: RouterName.DepartmentSearch,
           component: () => import("@/pages/DepartmentSearch.vue"),
         },
         {
           // NOTE: 项目维护
           path: "projectMaintain",
-          name: "projectMaintain",
+          name: RouterName.ProjectMaintain,
           component: () => import("@/pages/ProjectMaintain.vue"),
         },
         {
           // NOTE: 生成报表
           path: "generateReports",
-          name: "generateReports",
+          name: RouterName.GenerateReports,
           component: () => import("@/pages/GenerateReports.vue"),
         },
       ],
     },
     {
       path: "/home",
-      name: "home",
+      name: RouterName.Home,
       redirect: "/home/index",
       children: [
         {
           path: "index",
-          name: "index",
+          name: RouterName.Index,
           component: () => import("@/pages/ResponsiveTimeReport.vue"),
         },
       ],
     },
     {
       path: "/login",
-      name: "login",
+      name: RouterName.Login,
       component: () => import("@/pages/Login.vue"),
     },
   ],
