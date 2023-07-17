a9c8b7ce50c5be1a3212cffc30e28057ce14a171
凤之兮原 <1577310596@qq.com>
2023-07-17 13:32:32 +0800

    chore: add user UI for timeReport

---
+++ src/pages/TimeReport.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header />
+  <Content />
+  <Footer />
+</template>
+
+<script setup lang="ts">
+import Header from "@/views/ResponsiveTimeReport/Header.vue";
+import Content from "@/views/TimeReport/Content.tsx";
+import Footer from "@/views/ResponsiveTimeReport/Footer.vue";
+</script>
+
+<style scoped></style>
