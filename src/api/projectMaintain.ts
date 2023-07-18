f09d9ed435f710b2af2585cc5c9d6f552c389db8
凤之兮原 <1577310596@qq.com>
2023-07-18 08:30:35 +0800

    feat: add ADD and UPDATE api for projectMaintain

--- src/api/projectMaintain.ts
+++ src/api/projectMaintain.ts
@@ -3,11 +3,27 @@ import { http } from ".";
 
 const enum AxiosAPIPath {
   GETALL = "/getAllProjectList",
+  ADD = "/addProject",
+  UPDATE = "/updateProject",
   REMOVE = "/deleteProject",
 }
 // NOTE: 获取所有的人员查询信息
 export function fetchAllProject() {
   return http.get(AxiosAPIPath["GETALL"]);
+}
+
+// NOTE: 添加项目
+export function addProject(values) {
+  const res = http.post(AxiosAPIPath["ADD"], values);
+  message.success("添加成功");
+  return res;
+}
+
+// NOTE: 编辑项目
+export function updateProject(values) {
+  const res = http.put(AxiosAPIPath["UPDATE"], values);
+  message.success("更新成功");
+  return res;
 }
 
 // NOTE: 根据 Id 删除项目新息
