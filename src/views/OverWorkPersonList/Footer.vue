33430575596d269d512075661be007b989ed5aaa
凤之兮原 <1577310596@qq.com>
2023-07-19 15:19:44 +0800

    feat: change footer

--- src/views/OverWorkPersonList/Footer.vue
+++ src/views/OverWorkPersonList/Footer.vue
@@ -1,8 +1,5 @@
 <template>
-  <div bg="blue-300" p="5" text="center lg white" font="bold">
-    &copy; 2023-2024 Syj & Zmt
+  <div bg="blue-500" p="5" text="center lg white" font="bold">
+    &copy; 2023-2024 Syj
   </div>
 </template>
-
-<script setup lang="ts">
-</script>
