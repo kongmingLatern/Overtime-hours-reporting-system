67d5f391291379a81275fde22811da507842270c
凤之兮原 <1577310596@qq.com>
2023-07-19 15:19:20 +0800

    chore: add 404 page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-result status="404" title="404" sub-title="对不起,您所访问的页面不存在">
+    <template #extra>
+      <a-button type="primary" @click="goTo(RouterName.HOME)"
+        >回到首页</a-button
+      >
+    </template>
+  </a-result>
+</template>
+
+<script lang="ts" setup>
+import { useGoto } from "@/composables";
+import { RouterName } from "@/router/RouterName";
+const { goTo } = useGoto();
+</script>
