c9245f7e0ec20dbaa82f0ada4b812aa2196bea56
凤之兮原 <1577310596@qq.com>
2023-07-19 15:19:00 +0800

    chore: change DatePicker

--- src/components/common/DatePicker.vue
+++ src/components/common/DatePicker.vue
@@ -1,16 +1,18 @@
 <template>
-  <a-date-picker show-time placeholder="请选择项目时间" @change="onChange" />
+  <a-date-picker placeholder="请选择项目时间" @change="onChange" />
 </template>
 
 <script setup lang="ts">
 import { useProjectMaintain } from "@/store";
 import dayjs, { Dayjs } from "dayjs";
+const { data, init } = useProjectMaintain();
 
-const onChange = (_: Dayjs, dateString: string) => {
-  const { data } = useProjectMaintain();
+const onChange = async (_: Dayjs, dateString: string) => {
+  await init();
   data.value = data.value.filter((item) => {
-    const start = dayjs(item.project_time).valueOf();
-
+    const start = dayjs(
+      dayjs(item.project_time).format("YYYY-MM-DD")
+    ).valueOf();
     const endTime = dayjs(dateString).valueOf();
 
     return start <= endTime;
