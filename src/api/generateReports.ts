ead6cc5d7154685a6e9c7d02aeb4527bde279f88
凤之兮原 <1577310596@qq.com>
2023-07-19 10:11:21 +0800

    feat: finish monthly and yearly report

--- src/api/generateReports.ts
+++ src/api/generateReports.ts
@@ -2,8 +2,28 @@ import { http } from ".";
 
 const enum AxiosAPIPath {
   GETALL = "/getAllReportList",
+  GETMONTHLY = "/monthlyOvertime",
+  GETYEARLY = "/yearlyOvertime",
 }
 // NOTE: 获取所有的人员查询信息
 export function fetchAllGenerateReports() {
   return http.get(AxiosAPIPath["GETALL"]);
+}
+
+// NOTE: 获取月度报表
+export function fetchMonthData(record) {
+  return http.get(AxiosAPIPath["GETMONTHLY"], {
+    params: {
+      job_number: record.job_number,
+    },
+  });
+}
+
+// NOTE: 获取年度报表
+export function fetchYearData(record) {
+  return http.get(AxiosAPIPath["GETYEARLY"], {
+    params: {
+      job_number: record.job_number,
+    },
+  });
 }
