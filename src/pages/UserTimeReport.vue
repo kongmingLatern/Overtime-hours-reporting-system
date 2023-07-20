174d0f2107dc8db60962dee1f5124d02ed215a12
凤之兮原 <1577310596@qq.com>
2023-07-20 10:06:58 +0800

    chore: rename timereport

---
+++ src/pages/UserTimeReport.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pb-75px">
+    <Header />
+    <Content />
+  </div>
+  <TarBar />
+</template>
+
+<script setup lang="ts">
+import Header from "@/views/ResponsiveTimeReport/Header.vue";
+import Content from "@/views/UserTimeReport/Content.tsx";
+import TarBar from "@/views/UserTimeReport/Tarbar.vue";
+</script>
+
+<style scoped></style>
