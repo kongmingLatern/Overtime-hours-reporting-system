96b1c871fc7fa82c9b6c80a928696c12493c3e4d
凤之兮原 <1577310596@qq.com>
2023-07-20 10:09:21 +0800

    chore: change UserTimeReport

---
+++ src/views/UserTimeReport/TarBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <a-row
+    class="bg-#f4f4f4 h-[75px]"
+    fixed
+    bottom-0
+    left-0
+    w-full
+    align="middle"
+    justify="center"
+  >
+    <a-col
+      v-for="item in tarbarList"
+      :key="item.key"
+      :class="item.isActive ? 'text-[#180290]' : 'text-[#afadad]'"
+      :span="24 / tarbarList.length"
+      text-center
+      @click="changeActive(item.key)"
+    >
+      <a-space direction="vertical" j-a-c :size="0">
+        <Icon :icon="item.icon" width="30" />
+        <span>{{ item.text }}</span>
+      </a-space>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts" setup>
+import { Icon } from "@iconify/vue";
+import { ref } from "vue";
+const activeKeys = ref<string>("");
+
+const tarbarList = ref([
+  {
+    key: "pending",
+    icon: "material-symbols:pending-actions",
+    text: "待审批",
+    link: "/pending",
+    isActive: true,
+  },
+  {
+    key: "my",
+    icon: "mdi:account",
+    text: "我的",
+    link: "/my",
+    isActive: false,
+  },
+]);
+
+const changeActive = (key: string) => {
+  activeKeys.value = key;
+
+  tarbarList.value.forEach((item) => {
+    item.isActive = item.key === key;
+  });
+};
+</script>
---
+++ src/views/UserTimeReport/helpers/index.ts
@@ -0,0 +1,80 @@
+import { useProjectMaintain } from "@/store";
+import { projectSelect } from "@/utils";
+import { reactive, ref, watch } from "vue";
+
+const currentJobNumber = ref("");
+const res = ref<any>({});
+
+async function getOptions(job_number) {
+  res.value = await projectSelect(
+    useProjectMaintain,
+    true,
+    "job_number",
+    job_number
+  );
+}
+
+export const formState = reactive({
+  job_name: "",
+  job_number: "",
+  start_time: "",
+  end_time: "",
+  over_time: "",
+  project_name: "",
+  report_time: "",
+  over_time_reason: "",
+});
+
+export const ruleState = ref({
+  job_name: {
+    type: "text",
+    label: "员工姓名",
+  },
+  job_number: {
+    type: "number",
+    label: "工号",
+    options: {
+      onChange: (e) => {
+        currentJobNumber.value = e;
+      },
+      onBlur: async () => {
+        await searchProjectName(currentJobNumber.value);
+      },
+    },
+  },
+  start_time: {
+    type: "date",
+    label: "加班开始时间",
+  },
+  end_time: {
+    type: "date",
+    label: "加班结束时间",
+  },
+  over_time: {
+    type: "readonly",
+    label: "加班时长(时)",
+  },
+  report_time: {
+    type: "date",
+    label: "填报时间",
+  },
+  project_name: {
+    type: "select",
+    label: "所属项目",
+    options: {
+      options: [],
+    },
+  },
+  over_time_reason: {
+    type: "text",
+    label: "加班事由",
+  },
+});
+
+async function searchProjectName(job_number) {
+  await getOptions(job_number);
+  Object.assign(
+    ruleState.value["project_name"].options.options,
+    res.value.options
+  );
+}
